2b248de05704724a086b53588061fcb1d4f707eb
YagovkinSI <sorpionupgrade@mail.ru>
2023-06-04 07:27:49 +0400

    [Feature-2] Use functional style for react component

--- YAGO.FantasyWorld.Web/ClientApp/src/components/Counter.tsx
+++ YAGO.FantasyWorld.Web/ClientApp/src/components/Counter.tsx
@@ -9,24 +9,19 @@ type CounterProps =
     typeof CounterStore.actionCreators &
     RouteComponentProps<{}>;
 
-class Counter extends React.PureComponent<CounterProps> {
-    public render() {
-        return (
-            <React.Fragment>
-                <h1>Счетчик</h1>
-
-                <p>Это простой пример компонента React.</p>
-
-                <p aria-live="polite">Текущее значение: <strong>{this.props.count}</strong></p>
-
-                <button type="button"
-                    className="btn btn-primary btn-lg"
-                    onClick={() => { this.props.increment(); }}>
-                    Увеличить
-                </button>
-            </React.Fragment>
-        );
-    }
+const Counter: React.FC<CounterProps> = (props) => {
+    return (
+        <React.Fragment>
+            <h1>Счетчик</h1>
+            <p>Это простой пример компонента React.</p>
+            <p aria-live="polite">Текущее значение: <strong>{props.count}</strong></p>
+            <button type="button"
+                className="btn btn-primary btn-lg"
+                onClick={() => { props.increment(); }}>
+                Увеличить
+            </button>
+        </React.Fragment>
+    );
 };
 
 export default connect(
